3422ae745427ad71af129138f14fb532f0aca148
Amaury R. Peralta Febles <amauryperalta28@gmail.com>
2017-12-15 22:55:18 -0400

    Se agregaron los controles para agregar un canto nuevo

---
+++ src/models/songs.model.ts
@@ -0,0 +1,9 @@
+export class song{
+  title:string;
+  eucharistMoment:number;
+
+  constructor(title:string, moment:number){
+        this.title = title;
+        this.eucharistMoment = moment;
+    }
+}
